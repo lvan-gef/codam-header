c39d149e527de59bcd4cb285b24892c60cb231d5
Luuk van Geffen <luxesound@MacBook-Pro-van-Luuk.local>
2025-02-04 22:13:52 +0100

    Changed name to capital name

--- plugin/autocmd.lua
+++ plugin/autocmd.lua
@@ -1,7 +1,7 @@
-vim.api.nvim_create_user_command("headerInsert", function()
+vim.api.nvim_create_user_command("HeaderInsert", function()
   require("codam-header").insert()
 end, {})
 
-vim.api.nvim_create_user_command("headerUpdate", function()
+vim.api.nvim_create_user_command("HeaderUpdate", function()
   require("codam-header").update()
 end, {})
