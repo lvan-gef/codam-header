0b608aad1d9fadb63b04bd7ecfb15cc2375da6bc
Luuk van Geffen <luxesound@MacBook-Pro-van-Luuk.local>
2025-02-04 14:10:17 +0100

    Revoke changes

--- lua/codam-header/init.lua
+++ lua/codam-header/init.lua
@@ -11,41 +11,52 @@
 -- ************************************************************************** --
 
 local header = require("codam-header.header")
+local comment = require("codam-header.comment")
 
 local M = {}
 
 -- default setting for the user
 local options = {
-  author = "marvin",
-  email = "marvin@codam.nl",
+	author = "marvin",
+	email = "marvin@codam.nl",
 }
 
 --- Merge user settings with defaults
 --- @param opts table
 M.setup = function(opts)
-  opts = opts or {}
-
-  for k, v in pairs(options) do
-    if opts[k] == nil then
-      opts[k] = v
-    end
-  end
-
-  options = opts
+	opts = opts or {}
+	for k, v in pairs(options) do
+		if opts[k] == nil then
+			opts[k] = v
+		end
+	end
+	options = opts
 end
 
 --- insert header
 --- @return boolean
 M.insert = function()
-  local buffnr = vim.api.nvim_get_current_buf()
-  return header.insert(options, buffnr)
+	local buffnr = vim.api.nvim_get_current_buf()
+	local com_sign = comment.Get_comment(buffnr)
+
+	if com_sign == nil then
+		return false
+	end
+
+	return header.Insert_header(options, com_sign, buffnr)
 end
 
 --- update header
 --- @return boolean
 M.update = function()
-  local buffnr = vim.api.nvim_get_current_buf()
-  return header.update(options, buffnr)
+	local buffnr = vim.api.nvim_get_current_buf()
+	local com_sign = comment.Get_comment(buffnr)
+
+	if com_sign == nil then
+		return false
+	end
+
+	return header.Update_header(options, com_sign, buffnr)
 end
 
 return M
